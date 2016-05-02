b75b50b81c1b2977b452b03948908b0addf1e634
Blady <michel_dijkstra1@hotmail.com>
2016-05-02 13:50:40 +0200

    Put dialogs into Canvas.

--- Assets/Scripts/Dialogs.cs
+++ Assets/Scripts/Dialogs.cs
@@ -1,18 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Dialogs : MonoBehaviour 
 {
     string[,] dialogText = new string[10,10]
-        {{"Hi Chief! Welcome to 2016. My name is PAOLO and I’m an Engineer, just like YOU.\nI will help you fulfill your mission.","In order to start your mission you need to develop a SPACE CENTER.\nStart by giving it a name.","","","","","","","",""},
+        {{"Hi Chief! Welcome to 2016. My name is PAOLO and I’m an Engineer, just like YOU.","I will help you fulfill your mission.","In order to start your mission you need to develop a SPACE CENTER.","Start by giving it a name.","","","","","",""},
     {"Good! Cool name!","","","","","","","","",""},
-    {"Here YOU have some MONEY to get started.\nMONEY is the primary currency.\nUse it wisely.","","","","","","","","",""},
-    {"This is the HEADQUARTERS. The base of all operations.","QUESTS are the TASKS YOU need to perform to execute your mission properly.\nGo ahead and have a look.","","","","","","","",""},
+    {"Here YOU have some MONEY to get started.","MONEY is the primary currency. Use it wisely.","","","","","","","",""},
+    {"This is the HEADQUARTERS. The base of all operations.","QUESTS are the TASKS YOU need to perform to execute your mission properly.","Go ahead and have a look.","","","","","","",""},
     {"Here you can find FACILITIES which you will need for your mission and upgrading the SPACE CENTER.","","","","","","","","",""},
     {"Good job! You completed your first TASK.","","","","","","","","",""},
-    {"RESEARCH POINTS are the secondary currency.\nThe better you learn, the more you get.","","","","","","","","",""},
+    {"RESEARCH POINTS are the secondary currency. The better you learn, the more you get.","","","","","","","","",""},
     {"Well done Chief! A SPACE EXHIBIT is a nice way of generating revenue.","","","","","","","","",""},
-    {"With three buildings set, now it’s time to mark your SPACE CENTER as YOURS!\nPlace a FLAG of your choice to show who’s in charge.","","","","","","","","",""},
+    {"With three buildings set, now it’s time to mark your SPACE CENTER as YOURS!","Place a FLAG of your choice to show who’s in charge.","","","","","","","",""},
         {"What a beautiful flag you have right there! Your SPACE CENTER looks so professional. Great!","","","","","","","","",""}};
 
     public bool talk = false, waitForInput = false;
@@ -22,6 +23,10 @@ public class Dialogs : MonoBehaviour
     public float currentTime;
     int numberToSay = 0, dialogNumberMain;
     public bool tutorial = false;
+
+    [SerializeField]
+    GameObject canvas;
+    GameObject tempCanvas;
 
     public GUIStyle dialogStyle;
     public Texture2D backgroundDialog;
@@ -35,6 +40,7 @@ public class Dialogs : MonoBehaviour
                 pos++;
                 currentTime = Time.time;
                 msg01 = output.Substring(0, pos);
+                tempCanvas.GetComponentInChildren<Text>().text = msg01;
             }
             
             if(msg01 == output && !waitForInput && Input.GetMouseButtonDown(0))
@@ -47,6 +53,7 @@ public class Dialogs : MonoBehaviour
                     {
                         GameObject.Find("Account").GetComponent<Account>().ChangeColliders(true);
                     }
+                    Destroy(tempCanvas);
                 }
                 else
                 {
@@ -57,6 +64,7 @@ public class Dialogs : MonoBehaviour
             if (msg01 != output && Input.GetMouseButtonDown(0))
             {
                 msg01 = output;
+                tempCanvas.GetComponentInChildren<Text>().text = msg01;
             }
         }
 
@@ -66,22 +74,15 @@ public class Dialogs : MonoBehaviour
             {
                 waitForInput = false;
                 pos = 0;
+                Destroy(tempCanvas);
                 ActivateTalking(dialogNumberMain, numberToSay);
             }
-        }
-    }
-    
-    void OnGUI()
-    {
-        if (talk)
-        {
-            GUI.DrawTexture(new Rect(0, Screen.height - Screen.height / 4, Screen.width, Screen.height / 4), backgroundDialog);
-            GUI.TextArea(new Rect(10, Screen.height - Screen.height / 5, Screen.width - 10, 150), msg01, dialogStyle);
         }
     }
 
     public void ActivateTalking(int dialogNumber, int number = 0)
     {
+        tempCanvas = Instantiate(canvas);
         talk = true;
         numberToSay = number;
         dialogNumberMain = dialogNumber;
