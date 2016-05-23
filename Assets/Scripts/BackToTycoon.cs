9ede4a86a4060914e3e733d07d7f5f1a02ce6f5e
Blady <michel_dijkstra1@hotmail.com>
2016-05-23 11:41:02 +0200

    Added skymap temp
    Added badge screens.

---
+++ Assets/Scripts/BackToTycoon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BackToTycoon : MonoBehaviour 
+{
+	public void Back()
+    {
+        SceneManager.LoadScene("_Main");
+    }
+}
